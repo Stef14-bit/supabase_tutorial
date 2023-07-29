eb72a422ddd702d2c2e40ee9ab841a1ed065a55d
stefan <stefan@stefan.com>
2023-07-29 11:04:59 +0200

    auth form fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import AuthForm from "./auth-form";
 
 export default function Home() {
-  return AuthForm;
+  return (
+    <div className="flex flex-col  h-full align-middle content-center">
+      <div className="col-6">
+        <h1 className="header">Supabase Auth + Storage</h1>
+        <p className="">
+          Experience our Auth and Storage through a simple profile management
+          example. Create a user profile and upload an avatar image. Fast,
+          simple, secure.
+        </p>
+      </div>
+      <div className="col-6 auth-widget">
+        <AuthForm />
+      </div>
+    </div>
+  );
 }
